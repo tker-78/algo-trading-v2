4fe2daa80534087e00327f409245da63ae0787f3
Takuya Kinoshita <kktak02@gmail.com>
2025-01-13 10:37:28 +0900

    adding test for database session

--- models/base_test.py
+++ models/base_test.py
@@ -6,3 +6,8 @@ def test_engine_connection():
     with engine.connect() as connection:
         result = connection.execute(text("SELECT 1"))
         assert result.scalar() == 1
+
+def test_session_scope():
+    with session_scope() as session:
+        result = session.execute(text("SELECT 1"))
+        assert result.scalar() == 1
