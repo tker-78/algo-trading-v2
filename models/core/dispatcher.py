8142044093416b49c50a57a0b57d94cd2d4b3f3e
Takuya Kinoshita <kktak02@gmail.com>
2025-02-20 06:45:35 +0900

    temporary stop to implement

--- models/core/dispatcher.py
+++ models/core/dispatcher.py
@@ -1,31 +1,5 @@
 
-from typing import cast, Any, Awaitable, Callable, Dict, Generator, List, Optional, Set, Tuple
-import abc
-import asyncio
-import contextlib
-import dataclasses
-import datetime
-import heapq
-import logging
-import platform
-import signal
 
-from . import dt, event, helpers
-
-logger = logging.getLogger(__name__)
-EventHandler = Callable[[event.Event], Awaitable[Any]]
-IdleHandler = Callable[[], Awaitable[Any]]
-SchedulerJob = Callable[[], Awaitable[Any]]
-
-@dataclasses.dataclass
-class EventDispatch:
-    event: event.Event
-    handlers: List[EventHandler]
-
-@dataclasses.dataclass(order=True)
-class ScheduleJob:
-    when: datetime.datetime
-    job: SchedulerJob = dataclasses.field(compare=False)
 
 
 
